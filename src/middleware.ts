533548d086c69bca32329ec260972c6c790a50bf
Vivian Li <viv@kinde.com>
2025-08-16 21:45:42 +1000

    fix: rm dashboard from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { withAuth } from '@kinde-oss/kinde-auth-nextjs/middleware';
 
 export default withAuth(async function middleware() {}, {
-  publicPaths: ['/', '/api/public', '/dashboard'],
+  publicPaths: ['/', '/api/public'],
 });
 
 export const config = {
